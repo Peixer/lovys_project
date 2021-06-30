6db0b12c926bf11b4ca39e024d1a149274fd9c20
Glaicon <gjpeixer@gmail.com>
2021-06-30 04:39:04 -0300

    Adding new endpoint to create availability in CandidateController and creating new tests

---
+++ LovysProject/TestProject/CandidateControllerTest.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Core.Calendar.Models;
+using Core.Calendar.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using Shouldly;
+using WebApp.Controllers;
+
+namespace TestProject
+{
+    public class CandidateControllerTest
+    {
+        private Mock<IAvailabilityRepository> availabilityRepositoryMock;
+        private Mock<IUserRepository> userRepositoryMock;
+        private CandidateController controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            availabilityRepositoryMock = new Mock<IAvailabilityRepository>();
+            userRepositoryMock = new Mock<IUserRepository>();
+
+            userRepositoryMock.Setup(x => x.FindUserByUsername(It.IsAny<string>()))
+                .Returns(Task.FromResult(new User()));
+            availabilityRepositoryMock.Setup(x => x.Insert(It.IsAny<Availability>()))
+                .Returns(Task.FromResult(true));
+            
+            controller = new CandidateController(availabilityRepositoryMock.Object, userRepositoryMock.Object);
+        }
+
+        [Test]
+        public async Task should_sucess()
+        {
+            ObjectResult result = (ObjectResult) await controller.Post(new Availability()
+            {
+                EndTime = "10pm", StartTime = "09pm", DayOfWeek = DayOfWeek.Monday
+            });
+
+            result.Value.ToString().ShouldBe("sucess");
+        }
+
+        [Test]
+        public async Task should_fail_must_insert_endTime()
+        {
+            ObjectResult result = (ObjectResult) await controller.Post(new Availability()
+            {
+                StartTime = "9pm", DayOfWeek = DayOfWeek.Monday
+            });
+
+            result.StatusCode.ShouldBe(400);
+            result.Value.ToString().ShouldContain("'End Time' must not be empty.");
+        }
+
+        [Test]
+        public async Task should_fail_wrong_format_endTime()
+        {
+            ObjectResult result = (ObjectResult) await controller.Post(new Availability()
+            {
+                EndTime = "19pm", StartTime = "11pm", DayOfWeek = DayOfWeek.Monday
+            });
+
+            result.StatusCode.ShouldBe(400);
+            result.Value.ToString().ShouldContain("'End Time' is not in the correct format.");
+        }
+
+        [Test]
+        public async Task should_fail_startTime_less_than_endTime()
+        {
+            ObjectResult result = (ObjectResult) await controller.Post(new Availability()
+            {
+                EndTime = "10pm", StartTime = "12pm", DayOfWeek = DayOfWeek.Monday
+            });
+
+            result.StatusCode.ShouldBe(400);
+            result.Value.ToString().ShouldContain("'Start Time' must be less than '10pm'.");
+        }
+    }
+}
--- LovysProject/WebApp/Controllers/CandidateController.cs
+++ LovysProject/WebApp/Controllers/CandidateController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Core.Calendar.Models;
+using Core.Calendar.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Validators;
 
 namespace WebApp.Controllers
 {
@@ -7,13 +12,33 @@ namespace WebApp.Controllers
     [Route("candidates")]
     public class CandidateController : ControllerBase
     {
+        private readonly IAvailabilityRepository _availabilityRepository;
+        private readonly IUserRepository _userRepository;
+
+        public CandidateController(IAvailabilityRepository availabilityRepository, IUserRepository userRepository)
+        {
+            _availabilityRepository = availabilityRepository;
+            _userRepository = userRepository;
+        }
+        
         [Authorize(Roles = "Candidate")]
         [HttpPost]
-        public string Post()
+        public async Task<IActionResult> Post(Availability availability)
         {
-            //Add candidate availabilities
-            return "sucess";
+            var validator = new AvailabilityValidator();
+            var validRes = await validator.ValidateAsync(availability);
+            if (!validRes.IsValid)
+            {
+                return new BadRequestObjectResult(validRes.ToString(","));
+            }
+
+            string username = User?.FindFirst(ClaimTypes.Name)?.Value;
+
+            availability.User = await this._userRepository.FindUserByUsername(username);
+            await _availabilityRepository.Insert(availability);
+            return Ok("sucess");
         }
+        
         [HttpGet]
         public string Get()
         {
