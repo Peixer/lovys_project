8e701a0c581c67da274467f0a3affabb644ffe97
Glaicon <gjpeixer@gmail.com>
2021-06-29 23:36:55 -0300

    Creating new controller interviewer and updating webapp project

---
+++ LovysProject/LovysProject/Controllers/InterviewerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LovysProject.Controllers
+{
+    [ApiController]
+    [Route("interviewers")]
+    public class InterviewerController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "sucess";
+        }
+    }
+}
