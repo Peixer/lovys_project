fc5814e82638d31bbb309616c2b5c03a2cb3cc7b
Glaicon <gjpeixer@gmail.com>
2021-06-29 23:36:34 -0300

    Creating core project to add classes about Models, Repositories..

---
+++ LovysProject/Core/Calendar/Models/Availability.cs
@@ -0,0 +1,9 @@
+namespace Core.Calendar.Models
+{
+    public class Availability
+    {
+        public string StartTime { get; set; }
+        public string IdUser { get; set; }
+        public string IdCandidate { get; set; }
+    }
+}
---
+++ LovysProject/Core/Calendar/Models/User.cs
@@ -0,0 +1,8 @@
+namespace Core.Calendar.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
